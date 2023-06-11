c1d2decf5032bbf351116b1b3a8eb1d70cebaff3
devcon <devcon@admantium.com>
2023-06-11 16:04:34 +0200

    Update: define terraform version

--- versions.tf
+++ versions.tf
@@ -1,16 +1,18 @@
 terraform {
+  required_version = ">=1.4.6"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.2.0"
+      version = ">=5.2.0"
     }
     tls = {
       source  = "hashicorp/tls"
-      version = "~> 4.0.4"
+      version = ">= 4.0.4"
     }
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.5.1"
+      version = ">= 3.5.1"
     }
   }
 }
